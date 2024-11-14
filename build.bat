8d8c4519c3e8fc2d6c33e57b53ba35fc257229c2
andiputraw <andiputraw925@gmail.com>
2024-11-15 06:09:58 +0700

    update bat

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ set CC=.\bin\tcc\tcc
 @REM set CC="/"
 
 echo "Compiling..."
-%CC% -vv -o out\TicTac -I./lib/raylib/include TicTac.c -L./lib/raylib/lib -lraylib -lopengl32 -lgdi32 -lwinmm
+%CC% -vv -o out\TicTac.exe -I./lib/raylib/include TicTac.c -L./lib/raylib/lib -lraylib -lopengl32 -lgdi32 -lwinmm
 echo "Compile Completed"
